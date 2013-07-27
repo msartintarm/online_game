0a0f2c54f0592db15d07dff3d71f2f06512d5d59
Michael Sartin-Tarm <msartintarm@gmail.com>
2013-07-27 15:03:07 -0500

    php router script auto-packs JS files into .js.gz

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$path = pathinfo($_SERVER["SCRIPT_FILENAME"]);
+if ($path["extension"] == "js") {
+
+  $old_file = $_SERVER["SCRIPT_FILENAME"];
+  $new_file = $old_file . ".gz";
+
+
+  $gz = gzopen ( $new_file, 'w9' );
+  gzwrite ( $gz, file_get_contents($old_file) );
+  gzclose ($gz);
+  header("Content-Type: application/javascript");
+  header("Content-Encoding: gzip");
+  readfile($new_file);
+
+} else {
+  return FALSE;
+}
+?>
