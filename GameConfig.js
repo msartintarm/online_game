f9714989babab522e1b21697f5c782afa9543b9e
Michael Sartin-Tarm <msartintarm@gmail.com>
2013-08-06 18:39:38 -0500

    seperated DIV manipulation and Game initialization.

--- GameConfig.js
+++ GameConfig.js
@@ -7,16 +7,17 @@
  */
 function GameConfig(game) {
 
+    //            CONFIGURATION
     // Figure out some way to convert this to document eventually
     var config = {
         "grid-size": 50,
         "textures": ["brick-texture", "heaven-texture", "rug-texture"],
-    // Origin URL, destination node, loop[, loop offset, loop time])
-    // These are at 120 BPM: 1 sec = 2 beats
-    // 0. Low-pass input detects movement, occuring on the half-beat; slightly below 0.25s
-    // 1. Non-looping sound, which will be triggered by the above sample
-    // 2. Non-looping sound, which will be triggered by the above sample
-    // 3. Rest of the song.
+        // Origin URL, destination node, loop[, loop offset, loop time])
+        // These are at 120 BPM: 1 sec = 2 beats
+        // 0. Low-pass input detects movement, occuring on the half-beat; slightly below 0.25s
+        // 1. Non-looping sound, which will be triggered by the above sample
+        // 2. Non-looping sound, which will be triggered by the above sample
+        // 3. Rest of the song.
         "audio": [["music/beats.mp3", "audio-low-pass", "loop", "1", "8"],
                   ["music/Game_Hi Hat_2.wav", "audio-output"],
                   ["music/Game_keys_2.wav", "audio-output"],
@@ -24,81 +25,188 @@ function GameConfig(game) {
         // Syntax of pieces: name, texture, array of x-y coordinate strings
         // String values can either be absolute, or relative to prev. strings
         // Can also specify loops of continuous increments
-        "piece-0": ["floor", "rug-texture", "1", "3", ["-22,-1", "20*(+2,+0)"]],
-        "piece-1": ["wall", "brick-texture", "1", "1", ["12,3", "+2,+1", "+2,+1", "+2,+1", "12*(+2,+0)",
-                                      "+4,-2", "4*(+2,+0)",
-                                      "-4,+2", "4*(-2,+0)",
-                                      "+4,-2", "4*(+2,+0)",
-                                      "-4,+2", "4*(-2,+0)"]],
+        "piece-0": ["floor", "rug-texture", "1", "3", ["-11,-1", "20*(+1,+0)"]],
+        "piece-1": ["wall", "brick-texture", "1", "1", ["6,3", "+1,+1", "+1,+1", "+1,+1", "12*(+1,+0)",
+                                                        "+4,-2", "4*(+2,+0)",
+                                                        "-4,+2", "4*(-2,+0)",
+                                                        "+4,-2", "4*(+2,+0)",
+                                                        "-4,+2", "4*(-2,+0)"]],
         "start-position": ["0", "300", "750"]
     };
 
-    var config_div = document.createElement("div");
-    config_div.style.fontSize = "20px";
-    document.getElementById("banner").appendChild(config_div);
+    //          DIV SETUP
 
-    var curr_div = config_div; //
+    // Define functions that construct the div elements, then call them.
+    this.setupDivs = function() {
 
-    this.TextBox = function(value, width) {
-        var text_x = document.createElement("textarea");
-        text_x.value = value;
-        text_x.style.width = width;
-        text_x.rows = 1;
-        curr_div.appendChild(text_x);
-    };
+        var config_div = document.createElement("div");
+        config_div.style.fontSize = "20px";
+        document.getElementById("banner").appendChild(config_div);
 
-    var swap_div;
+        var curr_div = config_div;
 
-    // create div with title, and leave it open.
-    // a button will be created that invokes it.
-    this.openDiv = function(title) {
+        this.Break = function() {
+            var breakz = document.createElement("div");
+            breakz.height = "1";
+            breakz.style.clear = "both";
+            curr_div.appendChild(breakz);
+        };
 
-        swap_div = document.createElement("div");
+        this.TextBox = function(value, width) {
+            var text_x = document.createElement("textarea");
+            text_x.value = value;
+            text_x.style.width = width;
+            text_x.rows = 1;
+            curr_div.appendChild(text_x);
+        };
 
-        var button = document.createElement("input");
-        button.type = "button";
-        button.className = "floating";
-        button.value = title;
-        // Function sets div to be hidden by default,
-       //   then returns a function that toggles it on / off
-        button.onclick = (function(s) {
-            s.display = "none";
-            return function() {
-                if(s.display === "none")
-                    s.display = "inline-block";
-                else s.display = "none";
+        this.Square = function(color) {
+            var d = document.createElement("div");
+            d.style.width = "4px";
+            d.style.height = "4px";
+            d.style.cssFloat = "left";
+            d.onclick = (function(s, c) {
+                s.clicked = false;
+                s.backgroundColor = c;
+                return function() {
+                    console.log(s.backgroundColor + " " + c);
+                    if (s.clicked === false) {
+                        s.backgroundColor = "#4455ff";
+                    }
+                    else s.backgroundColor = c;
+                    s.clicked = !s.clicked;
+                };
+            } (d.style, color));
+            curr_div.appendChild(d);
+        };
+
+        // create div with title, and leave it open.
+        // a button will be created that invokes it.
+        this.openDiv = function(title) {
+
+            curr_div = document.createElement("div");
+
+            var button = document.createElement("input");
+            button.type = "button";
+            button.className = "floating";
+            button.value = title;
+            // Function sets div to be hidden by default,
+            //   then returns a function that toggles it on / off
+            button.onclick = (function(s) {
+                s.display = "none";
+                return function() {
+                    if(s.display === "none")
+                        s.display = "inline-block";
+                    else s.display = "none";
+                };
+            } (curr_div.style));
+
+            config_div.appendChild(button);
+
+        };
+
+        this.closeDiv = function() {
+            config_div.appendChild(curr_div);
+            curr_div = config_div;
+        };
+
+        this.initMiscDiv = function() {
+            this.openDiv("Misc");
+            if (config["start-position"]) {
+                config["start-position"].forEach(function(x, i) {
+                    this.TextBox(x, "30px");
+                }, this);
+            }
+            this.closeDiv();
+        };
+
+        this.initTexturesDiv = function() {
+            this.openDiv("Textures:");
+            config["textures"].forEach (function(texture) {
+                this.TextBox(texture, "100%");
+            }, this);
+            this.closeDiv();
+        };
+
+        this.initAudioDiv = function(gl_audio) {
+            this.openDiv("Music:");
+            config["audio"].forEach (function(sound) { this.TextBox(sound[0], "99%"); }, this);
+            this.closeDiv();
+        };
+
+        this.initPieceDiv = function(piece_name) {
+
+            var p0 = config[piece_name];
+            this.openDiv("Piece '" + p0[0] + "':");
+
+            var tex = p0[1];
+            tex = (tex === "brick-texture")? BRICK_TEXTURE:
+                (tex === "heaven-texture")? HEAVEN_TEXTURE:
+                (tex === "rug-texture")? RUG_TEXTURE: null;
+
+            var locs = {};
+
+            var mark = function(x,y) {
+                if(!locs[x]) { locs[x] = {}; }
+                locs[x][y] = 1;
             };
-        } (swap_div.style));
 
-        config_div.appendChild(button);
+            var coords = p0[4];
 
-        curr_div = swap_div;
+            var x = 0;
+            var y = 0;
+            for(var i = 0; i < coords.length; ++i) {
+
+                var termA = /(?:([0-9]+)\*\()?([-+])*([0-9]+)\,([-+])*([0-9]+)\)?/;
+                var zzap = termA.exec(coords[i]);
+                // Index 1: loop count (opt); 2, 4: inc sign (opt); 3, 5: value (non-opt)
+                var loop = (zzap[1])? zzap[1]: 1;
+
+                //            if (zzap[2]) this.TextBox(zzap[2], "78%");
+                //            this.TextBox(zzap[3], "20%");
+
+                for(var j = 0; j < loop; ++j) {
+                    // Either char doesn't exist, is a '-' (decrement), or is  a '+' (increment)
+                    x = (!zzap[2])? parseInt(zzap[3]):
+                        (zzap[2] === "-")? x - parseInt(zzap[3]): x + parseInt(zzap[3]);
+                    y = (!zzap[4])? parseInt(zzap[5]):
+                        (zzap[4] === "-")? y - parseInt(zzap[5]): y + parseInt(zzap[5]);
+                    mark(x,y);
+                }
+
+            }
+            for(var b = 10; b > -3; --b) {
+                this.Break();
+                for(var a = -8; a < 30; ++a) {
+                    if (locs[a] && locs[a][b]) this.Square("#66ff66");
+                    else this.Square("#ff4433");
+                }
+            }
+            this.closeDiv();
+        };
+
+        this.initPieceDiv("piece-0");
+        this.initPieceDiv("piece-1");
+        this.initAudioDiv();
+        this.initTexturesDiv();
+        this.initMiscDiv();
     };
 
-    this.closeDiv = function() {
-        config_div.appendChild(curr_div);
-        curr_div = config_div;
-    };
+    this.setupDivs();
 
     this.initMisc = function() {
-        this.openDiv("Misc");
         if (config["grid-size"]) game.grid = config["grid-size"];
         if (config["start-position"]) {
-            var cf = config["start-position"];
             var vecz = [];
-            cf.forEach(function(x, i) {
+            config["start-position"].forEach(function(x, i) {
                 vecz[i] = parseInt(x);
-                this.TextBox(x, "30px");
             }, this);
             game.matrix.vTranslate(vecz);
        }
-        this.closeDiv();
     };
 
     this.initTextures = function() {
-        this.openDiv("Textures:");
         config["textures"].forEach (function(texture) {
-            this.TextBox(texture, "100%");
             var t =
                 (texture === "brick-texture")? BRICK_TEXTURE:
                 (texture === "heaven-texture")? HEAVEN_TEXTURE:
@@ -109,12 +217,10 @@ function GameConfig(game) {
             if (t) GLtexture.create(gl_, t);
             if (n) GLtexture.create(gl_, n);
         }, this);
-        this.closeDiv();
     };
 
     this.addAudio = function(gl_audio) {
         return function(sound) {
-            this.TextBox(sound[0], "99%");
             var web_node =
                 (sound[1] === "audio-low-pass")? gl_audio.low_pass:
                 (sound[1] === "audio-output")? gl_audio.web_audio.destination:
@@ -132,27 +238,23 @@ function GameConfig(game) {
     };
 
     this.initAudio = function(gl_audio) {
-
-        this.openDiv("Music:");
         config["audio"].forEach (this.addAudio(gl_audio), this);
-        this.closeDiv();
     };
 
     this.initPiece = function(arr, piece_name) {
         var p0 = config[piece_name];
-
-        var w = game.grid * parseInt(p0[2]);
-        var h = game.grid * parseInt(p0[3]);
-
-        this.openDiv("Piece '" + p0[0] + "':");
 
         var tex = p0[1];
             tex = (tex === "brick-texture")? BRICK_TEXTURE:
             (tex === "heaven-texture")? HEAVEN_TEXTURE:
             (tex === "rug-texture")? RUG_TEXTURE: null;
 
+        var w = game.grid * parseInt(p0[2]); // actually, width / 2
+        var h = game.grid * parseInt(p0[3]);
+
         var create = function(x,y) {
-	    var x2 = x * w;
+
+	    var x2 = x * w * 2;
 	    var y2 = y * h;
             var l = -1;
 	    arr.push(new Quad([x2-w,y2+h,l],
@@ -174,8 +276,8 @@ function GameConfig(game) {
             var loop = (zzap[1])? zzap[1]: 1;
 
 
-            if (zzap[2]) this.TextBox(zzap[2], "78%");
-            this.TextBox(zzap[3], "20%");
+//            if (zzap[2]) this.TextBox(zzap[2], "78%");
+//            this.TextBox(zzap[3], "20%");
 
             for(var j = 0; j < loop; ++j) {
                 // Either char doesn't exist, is a '-' (decrement), or is  a '+' (increment)
@@ -187,6 +289,5 @@ function GameConfig(game) {
             }
 
         }
-        this.closeDiv();
     };
 }
